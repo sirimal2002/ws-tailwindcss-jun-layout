5b64b72dcee3836dc18590287bdee9422715f5e3
siriwatknp <siriwatkunaporn@gmail.com>
2024-11-23 10:38:56 +0700

    fix static export config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "/learn-tailwindcss-jun-layout"
+      : "",
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/learn-tailwindcss-jun-layout"
+      : "",
 };
 
 export default nextConfig;
